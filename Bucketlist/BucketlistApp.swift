2b27452135298d5f4c1a62ccd7795c1d1d8cebcb
Omnivrr <Omnivrr@gmail.com>
2023-08-09 23:49:42 +0100

    Initial Commit

---
+++ Bucketlist/BucketlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketlistApp.swift
+//  Bucketlist
+//
+//  Created by Bukhari Sani on 09/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct BucketlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
