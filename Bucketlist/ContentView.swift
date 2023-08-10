c15256c0e2941b20473dea226e0bcf8e439418f2
Omnivrr <Omnivrr@gmail.com>
2023-08-10 19:36:59 +0100

    FaceID

--- Bucketlist/ContentView.swift
+++ Bucketlist/ContentView.swift
@@ -4,39 +4,43 @@
 //
 //  Created by Bukhari Sani on 09/08/2023.
 //
-import MapKit
+import LocalAuthentication
 import SwiftUI
 
-struct Location: Identifiable {
-    let id = UUID()
-    let name: String
-    let coordinate: CLLocationCoordinate2D
-}
-
 struct ContentView: View {
-    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span:
-        MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
-    
-    
-    let locations = [
-        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
-        Location(name: "Tower of London", coordinate:
-                    CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076)),
-        ]
-    
+    @State private var isUnlocked = false
     var body: some View {
-        Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
-            MapMarker(coordinate: location.coordinate)
-        }
-            .ignoresSafeArea()
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+            
         }
-        .padding()
+        .onAppear(perform: authenticate)
     }
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your data"
+            
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics,
+                                    localizedReason: reason) { success, authenticationError in
+                if success {
+                  isUnlocked = true
+                } else {
+                    //there was a problem
+                }
+            }
+        } else {
+            // no biometrics
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
