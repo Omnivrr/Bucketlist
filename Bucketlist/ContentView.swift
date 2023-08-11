b4703b0bba800383276c2d5cae13861f44014eb4
Omnivrr <Omnivrr@gmail.com>
2023-08-11 23:48:00 +0100

    v

--- Bucketlist/ContentView.swift
+++ Bucketlist/ContentView.swift
@@ -4,43 +4,17 @@
 //
 //  Created by Bukhari Sani on 09/08/2023.
 //
-import LocalAuthentication
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isUnlocked = false
     var body: some View {
         VStack {
-            if isUnlocked {
-                Text("Unlocked")
-            } else {
-                Text("Locked")
-            }
-            
-        }
-        .onAppear(perform: authenticate)
-    }
-    func authenticate() {
-        let context = LAContext()
-        var error: NSError?
-        
-        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-            let reason = "We need to unlock your data"
-            
-            
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics,
-                                    localizedReason: reason) { success, authenticationError in
-                if success {
-                  isUnlocked = true
-                } else {
-                    //there was a problem
-                }
-            }
-        } else {
-            // no biometrics
-        }
-    }
     
+            Text("Hello, world!")
+        }
+        .padding()
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
